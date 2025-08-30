87ea660f78613c667c8ef779c035bf3b647779a5
User_2022 <igusa@mail.ru>
2025-08-30 19:57:49 +0300

    hydrogram2

---
+++ main_userbot.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+import os
+from datetime import datetime
+from pathlib import Path
+from dotenv import load_dotenv
+
+from sqlalchemy import create_engine, Engine, desc
+from sqlalchemy.orm import Session
+
+from hydrogram import Client, filters, idle
+from hydrogram.types import Message
+
+from db_models import Base, FilesData
+
+WRONG_FILENAME_CHARS = '\\/*?:\'"'
+
+DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tmp')
+Path(DATA_DIR).mkdir(parents=True, exist_ok=True)
+
+engine: Engine = create_engine(f'sqlite:///{os.path.join(DATA_DIR, "files_data.sqlite")}')
+Base.metadata.create_all(engine)
+
+load_dotenv()
+if os.environ.get('API_HASH', '') == '':
+    print('\nERROR: API_HASH or ".env" file NOT FOUND !!!')
+    exit(1)
+
+bot = Client("stats",
+             int(os.environ.get('API_ID', 0)),
+             os.environ.get('API_HASH', ''),
+             in_memory=True,
+             session_string=os.environ.get('API_SESSION', ''),
+             phone_number=os.environ.get('API_PHONE', ''))
+
+
+def get_user_folder(message: Message) -> str:
+    return os.path.join(DATA_DIR, f'u{message.from_user.id}')
+
+
+def print_message(message: Message):
+    print('\n--------------------\n')
+    print(message)
+    print('\n--------------------\n')
+
+
+def clear_filename_chars(filename: str) -> str:
+    s = filename.replace('  ', ' ')
+    for character in WRONG_FILENAME_CHARS:
+        s = s.replace(character, '')
+    return s
+
+
+def get_short_file_name(message: Message, suffix: str = '') -> str:
+    short_file_name = ' - '.join([message.audio.performer.strip(), message.audio.title.strip()])
+
+    suffix = suffix.strip()
+    if suffix != '': short_file_name += '_' + suffix
+
+    short_file_name = clear_filename_chars(short_file_name)
+
+    if short_file_name == '':
+        short_file_name = clear_filename_chars(message.audio.file_unique_id)
+    short_file_name += '.' + message.audio.file_name.split('.')[-1]
+    return short_file_name
+
+
+async def progress(current, total):
+    print(f"{current * 100 / total:.1f}%")
+
+
+async def save_file_if_not_exists(short_file_name: str, message: Message) -> None:
+    user_folder = get_user_folder(message)
+    Path(user_folder).mkdir(parents=True, exist_ok=True)
+    full_file_name = os.path.join(user_folder, short_file_name)
+
+    if not os.path.exists(full_file_name):
+        await bot.download_media(message=message, file_name=full_file_name, progress=progress)
+
+
+@bot.on_message(filters.incoming | filters.private | filters.audio)
+async def save_audio(_, message: Message):
+    try:
+        # print_message(message)
+        with Session(autoflush=False, bind=engine) as db:
+            file_data = db.query(FilesData).filter(
+                FilesData.file_unique_id == message.audio.file_unique_id,
+                FilesData.user_id == message.from_user.id).first()
+            if file_data is not None:
+                await save_file_if_not_exists(file_data.short_file_name, message)
+                await message.reply('exists')
+            else:
+                short_file_name = get_short_file_name(message)
+                if db.query(FilesData).filter(
+                        FilesData.short_file_name == short_file_name,
+                        FilesData.user_id == message.from_user.id).first() is not None:
+                    short_file_name = get_short_file_name(message, message.audio.file_unique_id)
+
+                now = datetime.now()
+                print('|'.join([f'{now.hour}:{now.minute}:{now.second}',
+                                'saving',
+                                str(message.from_user.id),
+                                message.audio.file_name,
+                                message.audio.mime_type,
+                                str(message.audio.duration),
+                                str(message.audio.file_size),
+                                short_file_name]))
+                await save_file_if_not_exists(short_file_name, message)
+
+                file_data = FilesData()
+                file_data.user_id = message.from_user.id
+                file_data.file_unique_id = message.audio.file_unique_id
+                file_data.performer = message.audio.performer
+                file_data.title = message.audio.title
+                file_data.file_name = message.audio.file_name
+                file_data.mime_type = message.audio.mime_type
+                file_data.short_file_name = short_file_name
+                db.add(file_data)
+                db.commit()
+
+                await message.reply('saved')
+
+    except Exception as e:
+        print(e)
+        await message.reply(f'ERROR\n{e}')
+
+
+# @bot.on_message(filters.private | filters.command(["start","list"]))
+# async def _(_, message: Message):
+#     with (Session(autoflush=False, bind=engine) as db):
+#         records = db.query(FilesData.short_file_name) \
+#             .where(FilesData.user_id == message.from_user.id) \
+#             .order_by(desc(FilesData.created_at))
+#
+#         files = [r.short_file_name for r in records]
+#         if not files:
+#             await message.reply("No files")
+#         else:
+#             large_text = f'Files ({len(files)}):\n{"\n".join(files)}'
+#             await message.reply(large_text[:3000])
+
+
+# bot.run()
+async def main():
+    print('\n(Press Ctrl+C to stop this)')
+    await bot.start()
+    # s = bot.export_session_string()
+    # print(f"\n{s}\n")
+    await bot.send_message("me", "Started. UserBot")
+    await idle()
+    await bot.stop()
+
+
+bot.run(main())
